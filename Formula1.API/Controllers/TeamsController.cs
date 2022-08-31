66d25659957ede453e9ad70b2167bbcf2b253142
james.randerson <james.randerson@tribalgroup.com>
2022-08-31 14:39:23 +0100

    basic api set up

---
+++ Formula1.API/Controllers/TeamsController.cs
@@ -0,0 +1,31 @@
+﻿using Formula1.API.DataStore;
+using Formula1.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Formula1.API.Controllers
+{
+    [ApiController]
+    [Route("api/teams")]
+    public class TeamsController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<TeamsDto>> GetTeams()
+        {
+            return Ok(TeamsDataStore.GetData.Teams);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<TeamsDto> GetTeams(int id)
+        {
+            var teamToReturn = TeamsDataStore.GetData.Teams.FirstOrDefault(t => t.Id == id);
+
+            if (teamToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(teamToReturn);
+
+        }
+    }
+}
---
+++ Formula1.API/DataStore/TeamsDataStore.cs
@@ -0,0 +1,22 @@
+﻿using Formula1.API.Models;
+using System.Net.NetworkInformation;
+
+namespace Formula1.API.DataStore
+{
+    public class TeamsDataStore
+    {
+        public List<TeamsDto> Teams { get; set; }
+
+        public static TeamsDataStore GetData { get; } = new TeamsDataStore();
+
+        public TeamsDataStore()
+        {
+            Teams = new List<TeamsDto>()
+            {
+                new TeamsDto { Id = 1, Name = "Mercedes", Description = "Merc Racing Team" },
+                new TeamsDto { Id = 2, Name = "Red Bull", Description = "Red Bull Racing Team" },
+                new TeamsDto { Id = 3, Name = "Ferrari", Description = "Ferrari Racing Team" },
+            };
+        }
+    }
+}
